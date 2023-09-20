5255b65b382d6b9c39460d4f14251165545165ad
Thomas Wilke <thomas.wilke@aracom.de>
2023-09-20 16:37:17 +0200

    02 props

--- .coding/01_component/components/FoodCard/index.tsx
+++ .coding/01_component/components/FoodCard/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 
 const FoodCard: FC = () => {
     return (
-        <div className="w-96 h-40 flex items-center justify-center bg-gray-800 border rounded">
+        <div className="w-96 p-4 flex items-center justify-center bg-gray-800 border rounded flex-col">
             <h1 className="text-3xl font-bold text-white">Food</h1>
         </div>
     )
